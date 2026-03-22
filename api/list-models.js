295f139ff56535dd070a18904df71ecefbe762f5
Aditi Anapindi <aditi.anapindi@gmail.com>
2026-03-22 01:47:47 -0700

    Add list-models diagnostic endpoint

---
+++ api/list-models.js
@@ -0,0 +1,8 @@
+export default async function handler(req, res) {
+  const response = await fetch(
+    `https://generativelanguage.googleapis.com/v1beta/models?key=${process.env.GEMINI_API_KEY}`
+  );
+  const data = await response.json();
+  const names = (data.models || []).map(m => m.name);
+  res.status(200).json(names);
+}
